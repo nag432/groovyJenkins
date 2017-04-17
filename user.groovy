aa6892bc5aaa4dc043bd591394101f08eb39d2ce
nag432 <nag432@users.noreply.github.com>
2017-04-17 16:24:05 +0530

    Update user.groovy

--- user.groovy
+++ user.groovy
@@ -1,4 +1,4 @@
 #!/var/lib/jenkins/groovy-2.4.10/bin/groovy
-user = hudson.model.User.get('username')
+user = hudson.model.user.get('username')
 prop = user.getProperty(jenkins.security.ApiTokenProperty.class)
 println(prop.getApiToken())
